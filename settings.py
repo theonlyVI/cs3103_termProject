ba9da7290feeb85d97e747e4b96dfeb7a7416893
Aliasgar Kothari <akothari@remotelabm28.cs.unb.ca>
2023-03-25 17:16:38 -0300

    starting out

---
+++ settings.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python
+APP_HOST = 'cs3103.cs.unb.ca'
+APP_PORT =  49005
+APP_DEBUG = True
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'akothari'
+MYSQL_PASSWD = '5YQGP3wE'
+MYSQL_DB = 'akothari'
+
+SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+
+LDAP_HOST =  'ldap-student.cs.unb.ca'
